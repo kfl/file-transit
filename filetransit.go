4eda869acf4268f2008810bb4cbe3c8a45de8b8e
Ken Friis Larsen <ken@friislarsen.net>
2016-10-12 12:14:22 +0200

    Use cloud store rather than blobstore

--- filetransit.go
+++ filetransit.go
@@ -1,27 +1,31 @@
 package filetransit
 
 import (
-        "html/template"
-        "io"
-        "net/http"
+	"io"
+	"fmt"
+	"net/http"
+	"net/url"
+	"crypto/rand"
+	"encoding/base64"
 
-        "golang.org/x/net/context"
+	"golang.org/x/net/context"
 
-        "google.golang.org/appengine"
-        "google.golang.org/appengine/blobstore"
-        "google.golang.org/appengine/log"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/file"
+	"google.golang.org/appengine/log"
+
+	"cloud.google.com/go/storage"
+
 )
 
 func serveError(ctx context.Context, w http.ResponseWriter, err error) {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Header().Set("Content-Type", "text/plain")
-        io.WriteString(w, "Internal Server Error")
-        log.Errorf(ctx, "%v", err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Internal Server Error")
+	log.Errorf(ctx, "%v", err)
 }
 
-var rootTemplate = template.Must(template.New("root").Parse(rootTemplateHTML))
-
-const rootTemplateHTML = `<!DOCTYPE html>
+const rootHTML = `<!DOCTYPE html>
 <html>
   <head>
     <title>File Transit Storage</title>
@@ -44,7 +48,8 @@ const rootTemplateHTML = `<!DOCTYPE html>
   </head>
   <body>
     <h1>For all your file transit needs</h1>
-    <form method="POST" action="{{.}}" enctype="multipart/form-data">
+    <form method="POST" action="/upload" enctype="multipart/form-data">
+      Course: <input type="text" name="course" /> <br/>
       Upload File: <input type="file" name="file" /> <br />
       <input type="submit" name="submit" value="Let's Rock and Roll" />
     </form>
@@ -52,41 +57,84 @@ const rootTemplateHTML = `<!DOCTYPE html>
 </html>`
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-        ctx := appengine.NewContext(r)
-        uploadURL, err := blobstore.UploadURL(ctx, "/upload", nil)
-        if err != nil {
-                serveError(ctx, w, err)
-                return
-        }
-        w.Header().Set("Content-Type", "text/html")
-        err = rootTemplate.Execute(w, uploadURL)
-        if err != nil {
-                log.Errorf(ctx, "%v", err)
-        }
+	fmt.Fprint(w, rootHTML)
 }
 
-func handleServe(w http.ResponseWriter, r *http.Request) {
-        blobstore.Send(w, appengine.BlobKey(r.FormValue("blobKey")))
+func uniqueFilename(stats string, base string) string {
+	randBytes := make([]byte, 32)
+    rand.Read(randBytes)
+	pre := url.QueryEscape(stats)
+	if pre == "" {
+		pre = "default"
+	}
+    return pre+"/"+base64.RawURLEncoding.EncodeToString(randBytes)+"/"+url.QueryEscape(base)
 }
+
+var bucket string
 
 func handleUpload(w http.ResponseWriter, r *http.Request) {
-        ctx := appengine.NewContext(r)
-        blobs, _, err := blobstore.ParseUpload(r)
-        if err != nil {
-                serveError(ctx, w, err)
-                return
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+
+	ctx := appengine.NewContext(r)
+
+	f, fh, err := r.FormFile("file")
+	if err != nil {
+		msg := fmt.Sprintf("Could not get file: %v", err)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+	defer f.Close()
+
+	if bucket == "" {
+        var err error
+        if bucket, err = file.DefaultBucketName(ctx); err != nil {
+			log.Errorf(ctx, "failed to get default GCS bucket name: %v", err)
+			msg := fmt.Sprintf("failed to get default GCS bucket name: %v", err)
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
         }
-        file := blobs["file"]
-        if len(file) == 0 {
-                log.Errorf(ctx, "no file uploaded")
-                http.Redirect(w, r, "/", http.StatusFound)
-                return
-        }
-        http.Redirect(w, r, "/serve/?blobKey="+string(file[0].BlobKey), http.StatusFound)
+	}
+	
+	storageClient, err := storage.NewClient(ctx)
+	if err != nil {
+		msg := fmt.Sprintf("failed to create client: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		log.Errorf(ctx, msg) 
+		return
+	}
+	defer storageClient.Close()
+
+	filename := uniqueFilename(r.FormValue("course"), fh.Filename)
+	
+	obj := storageClient.Bucket(bucket).Object(filename) 
+	sw := obj.NewWriter(ctx)
+	if _, err := io.Copy(sw, f); err != nil {
+		msg := fmt.Sprintf("Could not write file: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		log.Errorf(ctx, msg) 
+		return
+	}
+
+	if err := sw.Close(); err != nil {
+		msg := fmt.Sprintf("Could not put file: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		log.Errorf(ctx, msg) 
+		return
+	}
+
+	obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader)
+
+	u, _ := url.Parse("/" + bucket + "/" + filename)
+	dst := fmt.Sprintf("https://storage.googleapis.com%s", u)
+	
+	http.Redirect(w, r, dst, http.StatusFound)
 }
 
+
 func init() {
-        http.HandleFunc("/", handleRoot)
-        http.HandleFunc("/serve/", handleServe)
-        http.HandleFunc("/upload", handleUpload)
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/upload", handleUpload)
 }
